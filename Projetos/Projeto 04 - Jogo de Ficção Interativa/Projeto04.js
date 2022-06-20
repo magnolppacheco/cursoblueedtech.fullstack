bba60c54c667735a3a18acc81e07a821e45a17e4
magnolppacheco <magnolppacheco@gmail.com>
2022-06-19 21:20:48 -0300

    usado function, object, array, for, if, while ...

--- Projetos/Projeto 04 - Jogo de Ficção Interativa/Projeto04.js
+++ Projetos/Projeto 04 - Jogo de Ficção Interativa/Projeto04.js
@@ -1,14 +1,3 @@
-/*
-- Variáveis para armazenar os status do personagem. (1,0)
-- Perguntas que alteram esses status do personagem. (1,0)
-- Laço de repetição que determinará os ciclos onde a história vai se passar. 
-  Determinar o que acontecerá ao final do ciclo e como ficarão os status. (1,0)
-- Variável para controle da passagem do tempo. (1,0)
-- Variáveis de controle dos status e situações da história. (1,0)
-- Condicionais para alterações dessas variáveis. (1,5)
-- Funções para executar cada uma das tarefas. (1,5)
-- Conteúdo de condicionais, laços, funções e objetos bem aplicados. (2,0)
-*/
 
 // Monstro 1: Orc, Monstro 2: Troll, Monstro 3: Lizard, Monstro 4: Dementador
 const prompt = require("prompt-sync")();
